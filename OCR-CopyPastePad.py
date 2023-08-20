8b254bfe391ce97a59b96575879f5a2b526796f0
FlyingFathead <86568670+FlyingFathead@users.noreply.github.com>
2023-08-20 18:54:33 +0300

    v0.08 / option for inverting image colors
    Added the user option to invert and re-process the image if the initial results are sub-optimal (in some text types, this helps).

--- OCR-CopyPastePad.py
+++ OCR-CopyPastePad.py
@@ -1,15 +1,14 @@
 # OCR-CopyPastePad //  https://github.com/FlyingFathead/OCR-CopyPastePad/
-# v0.07 // Aug 2023 // FlyingFathead + ghost code by ChaosWhisperer
-
+# v0.08 // Aug 2023 // FlyingFathead + ghost code by ChaosWhisperer
 
 import tkinter as tk
 from tkinter import filedialog, messagebox
-from PIL import Image, ImageTk, ImageGrab
+from PIL import Image, ImageTk, ImageGrab, ImageOps
 import pytesseract
 import cv2
 import numpy as np
 
-VERSION = "v0.07"  # Updated version
+VERSION = "v0.08"  # Updated version
 
 class OCRCopyPastePad:
     def __init__(self, root):
@@ -44,14 +43,43 @@ class OCRCopyPastePad:
         self.detect_button = tk.Button(self.root, text="Detect Text Areas", command=self.detect_text_areas_and_ocr)
         self.detect_button.pack(pady=10)
 
+        # Invert colors button
+        self.invert_button = tk.Button(self.root, text="Invert Colors", command=self.invert_colors)
+        self.invert_button.pack(pady=10)
+
+    def invert_colors(self):
+        # Convert the image to RGB mode if it's not already
+        if self.image.mode != 'RGB':
+            self.image = self.image.convert('RGB')
+            
+        # Invert the colors of the image
+        inverted_image = ImageOps.invert(self.image)
+        self.image = inverted_image
+        self.process_image(inverted_image)
+
     def preprocess_image(self, image):
+        # Convert to grayscale
+        gray = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2GRAY)
+        
+        # Apply adaptive thresholding
+        thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+        
+        # Convert back to PIL Image
+        image = Image.fromarray(thresh)
+        
+        # Resize for better OCR accuracy
+        image = image.resize((int(image.width * 1.5), int(image.height * 1.5)))
+        
+        return image
+
+    """ def preprocess_image(self, image):
         # Convert to grayscale
         image = image.convert('L')
         # Apply binary threshold
         image = image.point(lambda x: 0 if x < 128 else 255, '1')
         # Resize for better OCR accuracy
         image = image.resize((int(image.width * 1.5), int(image.height * 1.5)))
-        return image
+        return image """
 
     def load_image(self):
         file_path = filedialog.askopenfilename(title="Select an image", filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.bmp;*.tiff")])
@@ -75,6 +103,18 @@ class OCRCopyPastePad:
         except Exception as e:
             messagebox.showerror("Error", str(e))
 
+    def resize_image_for_display(self, image, max_width=500, max_height=400):
+        """Resize the image to fit within the specified dimensions."""
+        width, height = image.size
+        aspect_ratio = width / height
+        if width > max_width:
+            width = max_width
+            height = int(width / aspect_ratio)
+        if height > max_height:
+            height = max_height
+            width = int(height * aspect_ratio)
+        return image.resize((width, height))
+
     def process_image(self, image):
         # Preprocess the image for better OCR accuracy
         processed_image = self.preprocess_image(image)
@@ -84,8 +124,11 @@ class OCRCopyPastePad:
         self.text_area.delete(1.0, tk.END)
         self.text_area.insert(tk.END, ocr_text)
 
-        # Display the original image
-        photo = ImageTk.PhotoImage(image)
+        # Resize the original image for display
+        display_image = self.resize_image_for_display(image)
+
+        # Display the resized original image
+        photo = ImageTk.PhotoImage(display_image)
         self.image_label.config(image=photo, text="")
         self.image_label.image = photo
 
